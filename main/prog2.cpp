cc8815ada61f909334ef253c4bb614a0bccea2a3
Sri Charan K <royalcharan2002@gmail.com>
2023-02-14 22:53:44 +0530

    Jenkinsfile made

---
+++ main/prog2.cpp
@@ -0,0 +1,10 @@
+//File: prog2.cpp
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout<<"Hello there, I am Obi Wan Kenobi\n";
+    cout<<"I am a Jedi Knight\n";
+    cout<<"May the force be with you\n";
+}
