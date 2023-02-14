f51a3ca9fc59eea99c0a985500036a6f6555c75f
Sri Charan K <royalcharan2002@gmail.com>
2023-02-14 21:04:48 +0530

    Commit from Bash

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I, Sri Charan has successfully built and run using Jenkins\n";
+  return 0;
+}
