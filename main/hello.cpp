d9f2b8d258d9d1885116c43aee74cbbd8aac4b76
Kanumuri Sri Charan <74090239+SriCharanK2002@users.noreply.github.com>
2023-02-15 19:57:28 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void)
 {
-  cout << "Hello, World\n";
+//   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I, Sri Charan has successfully built and run using Jenkins\n";
   cout << "I am an Indian\n";
